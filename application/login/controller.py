b5b3ae8b73c560d5124db919b24eddfc6abbcb17
notebook <notebook>
2021-05-06 19:36:57 +0300

    fixed log in for case if user doesn't exists

--- application/login/controller.py
+++ application/login/controller.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 from werkzeug.security import check_password_hash
 
 from application.db_model import User
@@ -11,10 +11,12 @@ def show_login_form():
     if form.validate_on_submit():
         enter_username = form.username.data
         enter_password = form.password.data
-        user = User.query.filter_by(username=enter_username).first()
-        if check_password_hash(user.password, enter_password):
-            return redirect(url_for('/to-do-list.greeting'))
+        try:
+            user = User.query.filter_by(username=enter_username).first()
+            check_password_hash(user.password, enter_password)
+        except AttributeError:
+            flash("Error")
         else:
-            return '<h3>Invalid</h3>'
+            return redirect(url_for('/to-do-list.greeting'))
 
     return render_template('form_login.html', html_form=form)
